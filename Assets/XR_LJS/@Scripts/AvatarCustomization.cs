662bc06cf2c1b32897f76bd9a0365ed010495eb4
ALOgame777 <gl7273@naver.com>
2024-10-07 13:25:56 +0900

    씬 생성

---
+++ Assets/XR_LJS/@Scripts/AvatarCustomization.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AvatarCustomization : MonoBehaviour
+{
+    public GameObject panel;
+    public SpriteRenderer head;
+    public Image spuareHeadDisplay;
+    public Color[] colors;
+    public int WhatColor;
+
+    void Update()
+    {
+        spuareHeadDisplay.color = head.color;
+        for (int i = 0; i < colors.Length; i++)
+        {
+            if(i == WhatColor)
+            {
+                head.color = colors[i];
+            }
+        }
+    }
+
+
+    public void ChangePanelState(bool state)
+    {
+        panel.SetActive(state);
+    }
+
+    public void ChangeHeadColor(int index)
+    {
+        WhatColor = index;
+    }
+}
