aab6ee8724f410e6eda4be766674726d7a9529c0
FortressforTrees <rlarlcks08@naver.com>
2024-10-17 01:33:01 +0900

    수요일

---
+++ Assets/XR_KGC/Scenes/KGC_Scripts/MakePlane.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class MakePlane : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        //가로세로 1인 쿼드를 가지고 바닥을 깔고싶다.
+        //쿼드 피벗 중심으로부터 X축으로 1씩 가면서 쿼드 깔기
+        //정해진 수 만큼 쿼드가 깔렸으면 Z축으로 1 올라간다음 반복
+        //그렇게 가로 세로 쿼드가 다 깔릴때까지 반복
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
