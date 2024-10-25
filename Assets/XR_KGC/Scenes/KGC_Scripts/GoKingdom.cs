a60825d5262879cb6dbe3bb14d63f008bf063557
k <rlarlcks08@naver.com>
2024-10-25 18:01:58 +0900

    레이캐스트오류해결

---
+++ Assets/XR_KGC/Scenes/KGC_Scripts/GoKingdom.cs
@@ -0,0 +1,42 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class GoKingdom : MonoBehaviour
+{
+    public bool inRange;
+
+    public GameObject player;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //플레이어가 일정거리안에 있으면 true로 바꿈
+        if (Vector3.Distance(transform.position, player.transform.position) < 35)
+        {
+            inRange = true;
+
+        }
+        else
+        {
+            inRange = false;
+        }
+
+    }
+
+    private void OnMouseDown()
+    {
+        if (inRange)
+        {
+
+            print("이동");
+        }
+
+
+    }
+}
---
+++ Assets/XR_KGC/Scenes/KGC_Scripts/ObjectClick.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+public class ObjectClick : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) // 마우스 왼쪽 버튼 클릭
+        {
+            DetectObjectOnClick();
+        }
+    }
+
+    void DetectObjectOnClick()
+    {
+        print("11111111111111111111111111111111111");
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        // Debug Ray를 Scene 창에서 시각적으로 확인하기 위해 충분히 길게 설정
+        Debug.DrawRay(ray.origin, ray.direction * 1000f, Color.red, 2f);
+
+        // Raycast 발사 및 충돌 여부 확인
+        //if (Physics.Raycast(ray, out hit, 1000f))
+        if (Physics.Raycast(ray.origin, ray.direction, out hit, 1000))
+        {
+            print("2222222222222222222222222222222222222222");
+            GameObject clickedObject = hit.collider.gameObject;
+            Debug.Log("Clicked on object: " + clickedObject.name); // 오브젝트 이름 출력
+        }
+        else
+        {
+            Debug.Log("No object was hit.");
+            print("3333333333333333333333333333333333");
+        }
+
+
+        
+    }
+}
