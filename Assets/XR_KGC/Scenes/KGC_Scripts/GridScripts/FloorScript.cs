e9f6af73174b8a839d37d2bfce6cc1d9475b1251
FortressforTrees <rlarlcks08@naver.com>
2024-11-06 02:26:23 +0900

    2D그리드만드는중

---
+++ Assets/XR_KGC/Scenes/KGC_Scripts/GridScripts/FloorScript.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class FloorScript : MonoBehaviour
+{
+    Collider col;
+
+    public bool occupied;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        col = GetComponent<CapsuleCollider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+          if (other.CompareTag("Set"))
+        {
+            occupied = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Set"))
+        {
+            occupied = false;
+        }
+    }
+}
