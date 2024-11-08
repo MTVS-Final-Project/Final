293eeed8780d8c45fe32fdc1d3bc29deef1e768e
ALOgame777 <gl7273@naver.com>
2024-11-08 14:16:18 +0900

    캐릭터 커스터마이징

--- Assets/XR_LJS/@Scripts/2_Scene/LevelNext.cs
+++ Assets/XR_LJS/@Scripts/2_Scene/LevelNext.cs
@@ -1,11 +1,64 @@
 using Photon.Pun;
+using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class LevelNext : MonoBehaviourPunCallbacks
 {
-    public void FirstScene()
+    private int selectedSprite = 0;  // 기본으로 선택된 스프라이트 인덱스
+    public List<Sprite> playerSprites = new List<Sprite>();  // 플레이어 스프라이트 리스트
+    public Button saveButton;  // 세이브 버튼
+    public Button loadButton;  // 로드 버튼
+
+    // Start 메서드: 버튼 클릭 이벤트 연결
+    void Start()
     {
-        SceneManager.LoadScene("Room_KGC");
+        // 세이브 버튼 클릭 시 Save 메서드 호출
+        saveButton.onClick.AddListener(SaveAndPrepareForSceneChange);
+
+        // 로드 버튼 클릭 시 Load 메서드 호출
+        loadButton.onClick.AddListener(LoadAndApplySprite);
+
+        // 디버그 로그: 게임 시작 시
+        Debug.Log("LevelNext 스크립트가 시작되었습니다. 초기 선택된 스프라이트 인덱스: " + selectedSprite);
+    }
+
+    // 세이브 버튼을 클릭하면 호출되는 메서드
+    public void SaveAndPrepareForSceneChange()
+    {
+        // 디버그 로그: 세이브 버튼 클릭 시
+        Debug.Log("세이브 버튼이 클릭되었습니다. 선택된 스프라이트 인덱스: " + selectedSprite);
+
+        // 세이브 경로
+        string filePath = Application.persistentDataPath + "/PlayerData.txt";  // 저장 경로 (쓰기 가능한 경로로 수정)
+        Debug.Log("선택된 스프라이트 인덱스(" + selectedSprite + ")를 파일에 저장합니다. 경로: " + filePath);
+
+        // 선택된 스프라이트 인덱스를 파일에 저장
+        File.WriteAllText(filePath, selectedSprite.ToString());
+
+        // 선택된 스프라이트를 현재 게임 오브젝트에 적용 (SpriteRenderer에 적용)
+        SpriteRenderer spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+        if (spriteRenderer != null && playerSprites.Count > selectedSprite)
+        {
+            spriteRenderer.sprite = playerSprites[selectedSprite];
+            Debug.Log("선택된 스프라이트가 적용되었습니다: " + playerSprites[selectedSprite].name);
+        }
+        else
+        {
+            Debug.LogWarning("SpriteRenderer가 없거나 선택된 스프라이트가 유효하지 않습니다.");
+        }
+
+        // 세이브 완료 후 씬 전환 준비
+        Debug.Log("세이브가 완료되었습니다. '다음 씬' 버튼을 클릭하여 씬을 전환하세요.");
+    }
+
+    // 로드 버튼을 클릭하면 호출되는 메서드
+    public void LoadAndApplySprite()
+    {
+        // 로드 후 씬 전환
+        Debug.Log("'Room_KGC' 씬으로 이동합니다.");
+        SceneManager.LoadScene("Room_KGC");  // "Room_KGC" 씬으로 이동
     }
 }
--- Assets/XR_LJS/@Scripts/2_Scene/PhotonNet.cs
+++ Assets/XR_LJS/@Scripts/2_Scene/PhotonNet.cs
@@ -1,8 +1,53 @@
 using UnityEngine;
 using Photon.Pun;
-
+using System.Collections.Generic;
+using System.IO;
+using Photon.Realtime;
 public class PhotonNet : MonoBehaviourPunCallbacks
 {
+    // 몸 부분
+    public SpriteRenderer bodyPart;
+    public Sprite[] bodyOption;
+
+    // 눈
+    public SpriteRenderer eyePart;
+    public Sprite[] eyeOption;
+    // 입
+    public SpriteRenderer mouthPart;
+    public Sprite[] mouthOption;
+    // 왼쪽 팔
+    public SpriteRenderer leftarmPart;
+    public Sprite[] leftarmOption;
+    
+    //오른쪽 팔
+    public SpriteRenderer rightarmPart;
+    public Sprite[] rightarmOption;
+
+    // 팬츠
+    public SpriteRenderer pantPart;
+    public Sprite[] pantOption;
+
+    // 헤어
+    public SpriteRenderer hairPart;
+    public Sprite[] hairOption;
+
+    //왼쪽 신발
+    public SpriteRenderer leftshoesPart;
+    public Sprite[] leftshoesOption;
+
+    // 오른쪽 신발
+    public SpriteRenderer rightshoesPart;
+    public Sprite[] rightshoesOption;
+
+    // 왼쪽 다리
+    public SpriteRenderer leftlegPart;
+    public Sprite[] leftlegOption;
+
+    // 오른쪽 다리
+    public SpriteRenderer rightlegPart;
+    public Sprite[] rightlegOption;
+
+
     public Transform catTransform; // 에디터에서 고양이 객체의 Transform을 연결합니다.
     public Transform circleTransform;
     void Start()
@@ -10,7 +55,6 @@ public class PhotonNet : MonoBehaviourPunCallbacks
         // RPC 보내는 빈도 설정
         PhotonNetwork.SendRate = 60;
         PhotonNetwork.SerializationRate = 60;
-
         if (catTransform != null)
         {
             // 고양이 위치 근처의 위치 계산
@@ -22,14 +66,17 @@ public class PhotonNet : MonoBehaviourPunCallbacks
             // 객체 생성
             GameObject playerInstance = PhotonNetwork.Instantiate("Player", spawnPosition, Quaternion.identity);
 
+            ddddd(playerInstance);
             // CatController의 player 변수에 자동 할당
             CatController.instance.player = playerInstance;
+
         }
         else if (circleTransform != null)
         {
             Vector3 spawnPos = GetRandomPositionNearCircle();
             Debug.Log("spawn pos : " + spawnPos);
             GameObject playerInstance = PhotonNetwork.Instantiate("Player", spawnPos, Quaternion.identity);
+            ddddd(playerInstance);
             Debug.Log("player : " + playerInstance);
         }
     }
@@ -70,9 +117,42 @@ public class PhotonNet : MonoBehaviourPunCallbacks
             return Vector3.zero;
         }
     }
-
-    void Update()
+    //private void UpdateSprite()
+    //{
+    //    if (part != null && Index >= 0 && Index < option.Length)
+    //    {
+    //        part.sprite = option[Index];
+    //    }
+    //}
+    void ddddd(GameObject player)
     {
-        // Update 로직 추가 가능
+        // Update 로직 추가 dddd
+
+        print(PhotonNetwork.LocalPlayer.CustomProperties["BodyUI"]);
+        print(PhotonNetwork.LocalPlayer.CustomProperties["EyesUI"]);
+        print(PhotonNetwork.LocalPlayer.CustomProperties["MouthUI"]);
+        print(PhotonNetwork.LocalPlayer.CustomProperties["LeftArmUI"]);
+        print(PhotonNetwork.LocalPlayer.CustomProperties["rightArmUI"]);
+        print(PhotonNetwork.LocalPlayer.CustomProperties["leftLegUI"]);
+        print(PhotonNetwork.LocalPlayer.CustomProperties["rightLegUI"]);
+        print(PhotonNetwork.LocalPlayer.CustomProperties["PantsUI"]);
+        print(PhotonNetwork.LocalPlayer.CustomProperties["HairUI"]);
+        print(PhotonNetwork.LocalPlayer.CustomProperties["leftShoesUI"]);
+        print(PhotonNetwork.LocalPlayer.CustomProperties["rightShoesUI"]);
+
+        // 상의는 몇번// 그거에 맞춰서 상의를 바꾼다
+        player.transform.Find("Body1").GetComponent<SpriteRenderer>().sprite = bodyOption[(int)PhotonNetwork.LocalPlayer.CustomProperties["BodyUI"]];
+        player.transform.Find("eye").GetComponent<SpriteRenderer>().sprite = eyeOption[(int)PhotonNetwork.LocalPlayer.CustomProperties["EyesUI"]];
+        player.transform.Find("mouth").GetComponent<SpriteRenderer>().sprite = mouthOption[(int)PhotonNetwork.LocalPlayer.CustomProperties["MouthUI"]];
+        player.transform.Find("leftArm1").GetComponent<SpriteRenderer>().sprite = leftarmOption[(int)PhotonNetwork.LocalPlayer.CustomProperties["LeftArmUI"]];
+        player.transform.Find("rightArmWhite").GetComponent<SpriteRenderer>().sprite = rightarmOption[(int)PhotonNetwork.LocalPlayer.CustomProperties["rightArmUI"]];
+        player.transform.Find("leftlegblack").GetComponent<SpriteRenderer>().sprite = leftlegOption[(int)PhotonNetwork.LocalPlayer.CustomProperties["leftLegUI"]];
+        player.transform.Find("rightlegblack").GetComponent<SpriteRenderer>().sprite = rightlegOption[(int)PhotonNetwork.LocalPlayer.CustomProperties["rightLegUI"]];
+        player.transform.Find("blackPant").GetComponent<SpriteRenderer>().sprite = pantOption[(int)PhotonNetwork.LocalPlayer.CustomProperties["PantsUI"]];
+        player.transform.Find("hairblack").GetComponent<SpriteRenderer>().sprite = hairOption[(int)PhotonNetwork.LocalPlayer.CustomProperties["HairUI"]];
+        player.transform.Find("leftshoesblack").GetComponent<SpriteRenderer>().sprite = leftshoesOption[(int)PhotonNetwork.LocalPlayer.CustomProperties["leftShoesUI"]];
+        player.transform.Find("rightshoesblack").GetComponent<SpriteRenderer>().sprite = rightshoesOption[(int)PhotonNetwork.LocalPlayer.CustomProperties["rightShoesUI"]];
+
+        
     }
 }
