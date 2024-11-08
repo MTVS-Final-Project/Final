3ccf58231a787f3bc4f9c18eaf31c3e1c609d3c2
k <rlarlcks08@naver.com>
2024-11-08 20:05:28 +0900

    가구배치기능 수정

--- Assets/XR_KGC/Scenes/KGC_Scripts/GridScripts/GaguCheck.cs
+++ Assets/XR_KGC/Scenes/KGC_Scripts/GridScripts/GaguCheck.cs
@@ -1,3 +1,4 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class GaguCheck : MonoBehaviour
@@ -5,7 +6,12 @@ public class GaguCheck : MonoBehaviour
     public bool onGagu;
     public bool onGround;
 
+    public PlaceButtonScript buttonScript;
+    public SpriteRenderer sr; //스프라이트 색 바꾸기
+
     public Vector3 snapPos;
+
+   // public bool findButtonScript;
 
    // public DragObject dragObject;
     
@@ -14,18 +20,38 @@ public class GaguCheck : MonoBehaviour
     void Start()
     {
        // dragObject = transform.parent.GetComponent<DragObject>();
+       buttonScript = GameObject.Find("PlaceCanvas").GetComponent<PlaceButtonScript>();
+       sr = transform.parent.GetChild(1).GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (buttonScript == null)
+        {
+            buttonScript = GameObject.Find("PlaceCanvas").GetComponent<PlaceButtonScript>();
+
+        }
+
+        buttonScript.gaguOnGagu = onGagu;
+
+        if (onGagu)
+        {
+            sr.color = new Color(255f / 255f, 0f / 255f, 0f / 255f, 150f / 255f);
+            //sr.color = new Color32(255, 0, 0, 150);
+        }
+        else
+        {
+            sr.color = Color.white;
+        }
+
     }
     private void OnTriggerStay(Collider other)
     {
         if ((other.CompareTag("Set")))
         {
                 onGagu = true;
+           // buttonScript.gaguOnGagu = true;
         }
         
         if (other.CompareTag("Ground"))
@@ -40,6 +66,7 @@ public class GaguCheck : MonoBehaviour
         if (other.CompareTag("Set"))
         {
             onGagu = false;
+            //buttonScript.gaguOnGagu = false;
         }
         if (other.CompareTag("Ground"))
         {
--- Assets/XR_KGC/Scenes/KGC_Scripts/PlaceButtonScript.cs
+++ Assets/XR_KGC/Scenes/KGC_Scripts/PlaceButtonScript.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class PlaceButtonScript : MonoBehaviour
@@ -5,18 +6,47 @@ public class PlaceButtonScript : MonoBehaviour
     DragObject dragObject;
 
     public GameObject selected;
+    public GameObject AlertText;
 
+    //public GameObject lines; //편집완료할때 꺼질애들
+    //public GameObject placeCanvas;
+    //public GameObject GaguCanvas;
+    //public GameObject RoomModify;
+    public ModifySetup ms; //참조해서 편집완료할때 끄는 함수 실행
+
+    public bool gaguOnGagu;
     public bool itemOn;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        AlertText = transform.GetChild(4).gameObject;
     }
 
     // Update is called once per frame
     void Update()
     {
+        //if (lines == null)
+        //{
+        //    lines = GameObject.Find("LineParent");
+        //}
+        //if (placeCanvas == null)
+        //{
+        //    placeCanvas = GameObject.Find("PlaceCanvas");
+        //}
+        //if (GaguCanvas == null)
+        //{
+        //    GaguCanvas = GameObject.Find("GaguCanvas");
+        //}
+        //if (RoomModify == null)
+        //{
+        //    RoomModify = GameObject.Find("RoomModify");
+        //}
+        if (ms == null)
+        {
+            ms = GameObject.Find("ModifyCanvas").GetComponent<ModifySetup>();
+        }
+
         if (selected == null)
         {
             itemOn = false;
@@ -66,7 +96,24 @@ public class PlaceButtonScript : MonoBehaviour
             selected.transform.eulerAngles = newRotation;
         }
     }
+    public void EndModify()
+    {
+        if (gaguOnGagu)
+        {
+            StartCoroutine(GOGAlert());
+        }
+        else
+        {
+            ms.EndModify();
+        }
+    }
+    public IEnumerator GOGAlert()
+    {
+        AlertText.SetActive(true);
 
+        yield return new WaitForSeconds(2);
 
+        AlertText.SetActive(false);
+    }
 }
 
