f0cbc3e858f2df1acb5b44887b163b32face1ac7
ALOgame777 <gl7273@naver.com>
2024-11-19 19:49:28 +0900

    20241119

--- Assets/XR_LJS/@Scripts/2_Scene/PhotonNet.cs
+++ Assets/XR_LJS/@Scripts/2_Scene/PhotonNet.cs
@@ -5,6 +5,7 @@ using Photon.Realtime;
 using System.Collections.Generic;
 using UnityEngine.Networking;
 using System.Net;
+using ExitGames.Client.Photon;
 
 public class PhotonNet : MonoBehaviourPunCallbacks
 {
@@ -68,7 +69,7 @@ public class PhotonNet : MonoBehaviourPunCallbacks
             if (playerInstance.GetComponent<PhotonView>().IsMine)
             {
                 LoadCharacterData();
-                CustomMizeGive(playerInstance);
+                CustomMizeGive(playerInstance);  // 내 캐릭터에 커스터마이징 적용
             }
             CatController.instance.player = playerInstance;
         }
@@ -79,11 +80,11 @@ public class PhotonNet : MonoBehaviourPunCallbacks
             if (playerInstance.GetComponent<PhotonView>().IsMine)
             {
                 LoadCharacterData();
-                CustomMizeGive(playerInstance);
+                CustomMizeGive(playerInstance);  // 내 캐릭터에 커스터마이징 적용
             }
         }
     }
-        Vector3 GetRandomPositionNearCat()
+    Vector3 GetRandomPositionNearCat()
     {
         float offsetX = Random.Range(-0.1f, 0.1f);
         float offsetY = Random.Range(-0.1f, 0.1f);
@@ -99,40 +100,50 @@ public class PhotonNet : MonoBehaviourPunCallbacks
 
     public void CustomMizeGive(GameObject player)
     {
+        // Body (몸통)
         if (PhotonNetwork.LocalPlayer.CustomProperties.TryGetValue("BodyUI", out object bodyIndex))
             player.transform.Find("Body1").GetComponent<SpriteRenderer>().sprite = bodyOption[(int)bodyIndex];
 
+        // Eyes (눈)
         if (PhotonNetwork.LocalPlayer.CustomProperties.TryGetValue("EyesUI", out object eyeIndex))
             player.transform.Find("eye").GetComponent<SpriteRenderer>().sprite = eyeOption[(int)eyeIndex];
 
+        // Mouth (입)
         if (PhotonNetwork.LocalPlayer.CustomProperties.TryGetValue("MouthUI", out object mouthIndex))
             player.transform.Find("mouth").GetComponent<SpriteRenderer>().sprite = mouthOption[(int)mouthIndex];
 
+        // Left Arm (왼쪽 팔)
         if (PhotonNetwork.LocalPlayer.CustomProperties.TryGetValue("LeftArmUI", out object leftArmIndex))
             player.transform.Find("leftArm").GetComponent<SpriteRenderer>().sprite = leftarmOption[(int)leftArmIndex];
 
+        // Right Arm (오른쪽 팔)
         if (PhotonNetwork.LocalPlayer.CustomProperties.TryGetValue("rightArmUI", out object rightArmIndex))
             player.transform.Find("rightArm").GetComponent<SpriteRenderer>().sprite = rightarmOption[(int)rightArmIndex];
 
+        // Left Leg (왼쪽 다리)
         if (PhotonNetwork.LocalPlayer.CustomProperties.TryGetValue("leftLegUI", out object leftLegIndex))
             player.transform.Find("leftLeg").GetComponent<SpriteRenderer>().sprite = leftlegOption[(int)leftLegIndex];
 
+        // Right Leg (오른쪽 다리)
         if (PhotonNetwork.LocalPlayer.CustomProperties.TryGetValue("rightLegUI", out object rightLegIndex))
             player.transform.Find("rightLeg").GetComponent<SpriteRenderer>().sprite = rightlegOption[(int)rightLegIndex];
 
+        // Pants (팬츠)
         if (PhotonNetwork.LocalPlayer.CustomProperties.TryGetValue("PantsUI", out object pantIndex))
             player.transform.Find("pants").GetComponent<SpriteRenderer>().sprite = pantOption[(int)pantIndex];
 
+        // Hair (헤어)
         if (PhotonNetwork.LocalPlayer.CustomProperties.TryGetValue("HairUI", out object hairIndex))
             player.transform.Find("hair").GetComponent<SpriteRenderer>().sprite = hairOption[(int)hairIndex];
 
+        // Left Shoe (왼쪽 신발)
         if (PhotonNetwork.LocalPlayer.CustomProperties.TryGetValue("leftShoesUI", out object leftShoesIndex))
             player.transform.Find("leftShoe").GetComponent<SpriteRenderer>().sprite = leftshoesOption[(int)leftShoesIndex];
 
+        // Right Shoe (오른쪽 신발)
         if (PhotonNetwork.LocalPlayer.CustomProperties.TryGetValue("rightShoesUI", out object rightShoesIndex))
             player.transform.Find("rightShoe").GetComponent<SpriteRenderer>().sprite = rightshoesOption[(int)rightShoesIndex];
     }
-
     private int GetUserId()
     {
         // PhotonNetwork.LocalPlayer.UserId는 string이므로 비교합니다.
@@ -162,6 +173,7 @@ public class PhotonNet : MonoBehaviourPunCallbacks
                                 int leftLegIndex, int rightLegIndex, int pantIndex, int hairIndex,
                                 int leftShoesIndex, int rightShoesIndex)
     {
+        // 커스터마이징 데이터를 로컬 플레이어에 저장
         var properties = new ExitGames.Client.Photon.Hashtable()
     {
         { "BodyUI", bodyIndex },
@@ -177,33 +189,84 @@ public class PhotonNet : MonoBehaviourPunCallbacks
         { "rightShoesUI", rightShoesIndex }
     };
 
-        // Custom Properties 업데이트
+        // CustomProperties 업데이트
         PhotonNetwork.LocalPlayer.SetCustomProperties(properties);
 
-        // 커스터마이징을 모든 클라이언트에 동기화
-        if (photonView.IsMine)
-        {
-            var customizationData = new APIManager.CustomizationData
-            {
-                skin = bodyIndex,
-                eye = eyeIndex,
-                mouth = mouthIndex,
-                leftArm = leftArmIndex,
-                rightArm = rightArmIndex,
-                leftLeg = leftLegIndex,
-                rightLeg = rightLegIndex,
-                pants = pantIndex,
-                hair = hairIndex,
-                leftShoe = leftShoesIndex,
-                rightShoe = rightShoesIndex
-            };
+        // 다른 클라이언트와 동기화
+        photonView.RPC("ApplyCustomization", RpcTarget.Others, bodyIndex, eyeIndex, mouthIndex, leftArmIndex, rightArmIndex,
+                       leftLegIndex, rightLegIndex, pantIndex, hairIndex, leftShoesIndex, rightShoesIndex);
 
-            int userId = GetUserId();
-            StartCoroutine(APIManager.Instance.UpdateCharacterData(userId, customizationData));
-        }
-
-        // 동기화 후 모든 클라이언트에 반영
+        // 즉시 반영
         CustomMizeGive(gameObject);
     }
 
+
+    [PunRPC]
+    public void ApplyCustomization(int bodyIndex, int eyeIndex, int mouthIndex, int leftArmIndex, int rightArmIndex,
+                               int leftLegIndex, int rightLegIndex, int pantIndex, int hairIndex,
+                               int leftShoesIndex, int rightShoesIndex)
+    {
+        // 전달받은 데이터로 커스터마이징 적용
+        var properties = new ExitGames.Client.Photon.Hashtable()
+    {
+        { "BodyUI", bodyIndex },
+        { "EyesUI", eyeIndex },
+        { "MouthUI", mouthIndex },
+        { "LeftArmUI", leftArmIndex },
+        { "rightArmUI", rightArmIndex },
+        { "leftLegUI", leftLegIndex },
+        { "rightLegUI", rightLegIndex },
+        { "PantsUI", pantIndex },
+        { "HairUI", hairIndex },
+        { "leftShoesUI", leftShoesIndex },
+        { "rightShoesUI", rightShoesIndex }
+    };
+
+        // CustomProperties 업데이트
+        PhotonNetwork.LocalPlayer.SetCustomProperties(properties);
+
+        // 캐릭터 반영
+        CustomMizeGive(gameObject);
+    }
+
+    public override void OnPlayerPropertiesUpdate(Player targetPlayer, ExitGames.Client.Photon.Hashtable changedProps)
+    {
+        if (targetPlayer == PhotonNetwork.LocalPlayer)
+        {
+            // 내 커스터마이징 정보를 업데이트
+            CustomMizeGive(gameObject);
+        }
+        else
+        {
+            // 다른 플레이어의 커스터마이징 정보를 업데이트
+            GameObject playerObject = FindPlayerObject(targetPlayer.ActorNumber);
+            if (playerObject != null)
+            {
+                ApplyCustomizationFromProperties(playerObject, changedProps);
+            }
+        }
+    }
+
+    private void ApplyCustomizationFromProperties(GameObject playerObject, ExitGames.Client.Photon.Hashtable props)
+    {
+        foreach (string key in props.Keys)
+        {
+            if (props.TryGetValue(key, out object value))
+            {
+                // key와 value를 사용해 커스터마이징 적용
+                // 예: playerObject.transform.Find(key).GetComponent<SpriteRenderer>().sprite = ...
+            }
+        }
+    }
+    private GameObject FindPlayerObject(int actorNumber)
+    {
+        foreach (var player in FindObjectsOfType<PhotonView>())
+        {
+            if (player.Owner.ActorNumber == actorNumber)
+            {
+                return player.gameObject;
+            }
+        }
+        return null;
+    }
 }
