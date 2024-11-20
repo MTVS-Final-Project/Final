72ceb4721e4931bb81fb85c426ad58867e14d8be
ALOgame777 <gl7273@naver.com>
2024-11-20 14:06:48 +0900

    조이스틱 추가

--- Assets/UJoystick/Example/Scrips/bl_ControllerExample.cs
+++ Assets/UJoystick/Example/Scrips/bl_ControllerExample.cs
@@ -1,28 +1,56 @@
 ﻿using UnityEngine;
+using Photon.Pun;
 
-public class bl_ControllerExample : MonoBehaviour {
+public class bl_ControllerExample : MonoBehaviourPunCallbacks
+{
+    [SerializeField] private bl_Joystick Joystick;
+    [SerializeField] private float Speed = 5f;
 
-    /// <summary>
-    /// Step #1
-    /// We need a simple reference of joystick in the script
-    /// that we need add it.
-    /// </summary>
-	[SerializeField]private bl_Joystick Joystick;//Joystick reference for assign in inspector
+    private Rigidbody2D rb;
+    private PhotonView photonView;
 
-    [SerializeField]private float Speed = 5;
+    void Awake()
+    {
+        // 씬에서 조이스틱 찾아서 자동 할당
+        if (Joystick == null)
+        {
+            Joystick = FindObjectOfType<bl_Joystick>();
+        }
+    }
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        photonView = GetComponent<PhotonView>();
+
+        if (rb != null)
+        {
+            rb.interpolation = RigidbodyInterpolation2D.Interpolate;
+            rb.collisionDetectionMode = CollisionDetectionMode2D.Continuous;
+        }
+    }
 
     void Update()
     {
-        //Step #2
-        //Change Input.GetAxis (or the input that you using) to Joystick.Vertical or Joystick.Horizontal
-        float v = Joystick.Vertical; //get the vertical value of joystick
-        float h = Joystick.Horizontal;//get the horizontal value of joystick
+        if (!photonView.IsMine) return;
 
-        //in case you using keys instead of axis (due keys are bool and not float) you can do this:
-        //bool isKeyPressed = (Joystick.Horizontal > 0) ? true : false;
+        // 혹시 조이스틱을 찾지 못했다면 다시 한번 시도
+        if (Joystick == null)
+        {
+            Joystick = FindObjectOfType<bl_Joystick>();
+            if (Joystick == null) return; // 조이스틱이 없으면 움직임 처리하지 않음
+        }
 
-        //ready!, you not need more.
-        Vector3 translate = (new Vector3(h, 0, v) * Time.deltaTime) * Speed;
-        transform.Translate(translate);
+        float horizontalInput = Joystick.Horizontal;
+        float verticalInput = Joystick.Vertical;
+
+        Vector2 movement = new Vector2(horizontalInput, verticalInput);
+
+        if (movement.magnitude > 1f)
+        {
+            movement.Normalize();
+        }
+
+        rb.linearVelocity = movement * Speed;
     }
 }
